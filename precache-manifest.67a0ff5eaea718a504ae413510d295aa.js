c2d557ba6d216d0254c4c5fc606577e904d7e5da
dmitry-honcharuk <dmitry-honcharuk@users.noreply.github.com>
2020-01-07 12:43:06 +0000

    publish

---
+++ precache-manifest.67a0ff5eaea718a504ae413510d295aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954a6d12741d140ca46416f56245f924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c16d3bb42578684f38fc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17737bef13b372d1f492",
+    "url": "/static/js/2.049f052f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.049f052f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c16d3bb42578684f38fc",
+    "url": "/static/js/main.e7ff20a6.chunk.js"
+  },
+  {
+    "revision": "1319be483d8bc9e40bbc",
+    "url": "/static/js/runtime-main.39bd5b45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
